25c58bf40f6aa435ffd484bd571b02988e0c97d7
Gordon-Allen <gordonmichaelallen@gmail.com>
2020-05-12 16:18:10 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// export default class App extends Component { 
+//   state = {count: 0}; 
+//     render() { 
+//       return ( 
+//         <section> 
+//         <h1>{this.state.count}</h1> 
+//         <button onClick={() => this.setState((prevState)=> 
+//         {
+//           return { 
+//             count: prevState.count + 1 
+//           }
+//         }
+//       )}> 
+//         Add 
+//         </button> 
+//         </section>); 
+//     } 
+// }
 
-export default App;
+
+//USING useState
+// import React, { useState } from 'react'; 
+
+// export default function App() { 
+//   const[count, setCount] = useState(0); 
+//       return ( 
+//         <section> 
+//           <h1>{count}</h1> 
+//           <button onClick={() => setCount(count+1)}> 
+//           Add 
+//           </button> 
+//         </section>); 
+
+// }
+
+
+
+// import React from 'react'; 
+
+// export default class App extends React.Component { 
+// constructor(props) { 
+// super(props); 
+// this.state = { 
+// count: 0 
+// }; 
+// } 
+
+// componentDidMount() { console.log("You clicked "+this.state.count+" times") } 
+// componentDidUpdate() { console.log("You clicked "+this.state.count+" times") } 
+
+// render() { 
+// return ( 
+// <div> 
+// <p>You clicked {this.state.count} times</p> 
+// <button onClick={() => this.setState({ count: this.state.count + 1 })}> 
+// Click me 
+// </button> 
+// </div> 
+// ); 
+// } 
+// }
+
+
+// USING useState & useEffect
+import React, {useState, useEffect} from 'react'; 
+
+export default function App() { 
+    const[count, setCount] = useState(0); 
+
+    useEffect(() => {
+        console.log("Amount of times clicked: " + count)
+    })
+        return ( 
+          <section> 
+            <h1>{count}</h1> 
+            <button onClick={() => setCount(count+1)}> 
+            Add 
+            </button> 
+          </section>
+        ); 
+  }
